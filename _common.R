40007e030e85ccebc33270133188b538ae1e6cc9
Luke Johnston <lwjohnst@gmail.com>
2019-11-14 17:04:58 +0100

    bookdown setup files

---
+++ _common.R
@@ -0,0 +1,31 @@
+
+library(RefManageR)
+library(NHANES)
+library(knitr)
+library(tidyverse)
+library(DiagrammeR)
+library(htmltools)
+library(webshot)
+
+knitr::opts_chunk$set(
+    comment = "#>",
+    collapse = TRUE,
+    warning = FALSE,
+    dpi = 72,
+    fig.width = 6,
+    fig.height = 6,
+    fig.align = "center"
+)
+
+BibOptions(
+    check.entries = FALSE,
+    bib.style = "authoryear",
+    cite.style = "authoryear",
+    style = "markdown",
+    super = TRUE
+    # hyperlink = FALSE,
+)
+
+options(knitr.table.format = "html")
+
+bib <- ReadBib("references.bib", check = FALSE)
