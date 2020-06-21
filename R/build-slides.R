3f5364b45fd6673dc13bc2987fc6fbec0b7e7157
Luke Johnston <lwjohnst@gmail.com>
2020-06-21 18:33:40 +0200

    rearranged build code to move slides over into website folder. Resolves #41, resolves #40

--- R/build-slides.R
+++ R/build-slides.R
@@ -1,8 +1,7 @@
 
 rmarkdown::render(
     input = here::here("slides/introduction.Rmd"),
-    output_file = "slides-introduction.html",
-    output_dir = here::here("public/"),
     knit_root_dir = here::here("."),
     quiet = TRUE
 )
+
