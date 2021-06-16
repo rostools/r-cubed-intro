98d070e8840ae251caa25a9d3d0baa9bf26d3bc3
Luke W. Johnston <lwjohnst@gmail.com>
2021-06-16 13:01:21 +0200

    use map and fs for building slides

--- R/build-slides.R
+++ R/build-slides.R
@@ -1,22 +1,5 @@
-# install.packages("bibtex")
 # Convert slides from Rmd to HTML -----------------------------------------
 
-rmarkdown::render(
-    input = here::here("slides/introduction.Rmd"),
-    # knit_root_dir = here::here("."),
-    quiet = TRUE
-)
-
-rmarkdown::render(
-    input = here::here("slides/collaboration-teamwork.Rmd"),
-    # Not use this because links to images need to translate to website
-    # knit_root_dir = here::here("."),
-    quiet = TRUE
-)
-
-rmarkdown::render(
-    input = here::here("slides/open-reproducible-science-era.Rmd"),
-    # Not use this because links to images need to translate to website
-    # knit_root_dir = here::here("."),
-    quiet = TRUE
-)
+library(magrittr)
+fs::dir_ls("slides", glob = "*.Rmd") %>%
+    purrr::map(rmarkdown::render, quiet = TRUE)
