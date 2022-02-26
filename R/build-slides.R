b6158df4277b63944f05e0c083c17ffed46cd66c
Luke W. Johnston <lwjohnst@gmail.com>
2022-02-26 12:45:32 +0100

    needed to callr to fix an annoying issue with refmanager and knitting multiple docs at once

--- R/build-slides.R
+++ R/build-slides.R
@@ -1,5 +1,10 @@
 # Convert slides from Rmd to HTML -----------------------------------------
 
-library(magrittr)
-fs::dir_ls(here::here("slides"), glob = "*.Rmd") %>%
-    purrr::map(rmarkdown::render, quiet = TRUE)
+# Need to do this for RefManageR to make citations properly
+render_separate_env <- function(file) {
+    callr::r(function(file) rmarkdown::render(file, quiet = TRUE),
+             args = list(file = file))
+}
+
+fs::dir_ls(here::here("slides"), glob = "*.Rmd") |>
+    purrr::walk(render_separate_env)
