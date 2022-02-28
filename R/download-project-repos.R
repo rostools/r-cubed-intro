b67124abd11368452a8d9a8f66dfa4f5e79b27ee
Luke W. Johnston <lwjohnst@gmail.com>
2022-02-28 21:59:11 +0100

    revise script to pull and clone repos

--- R/download-project-repos.R
+++ R/download-project-repos.R
@@ -8,10 +8,25 @@ stop("Prevent accidental sourcing.")
 gitcreds::gitcreds_set()
 ghclass::github_set_token(gitcreds::gitcreds_get(use_cache = FALSE)$password)
 
-# use this? withr::local_dir()
-team_repos <- org_repos("r-cubed-2021-06")
+# Use this if you didn't run the create-teams.R script on the computer used for
+# the course
+org_gh_course_name <- "r-cubed-2022-03"
 
-purrr::walk(team_repos, local_repo_clone)
+clone_team_projects <- function(repo_path, parent_directory) {
+    project_folder <- fs::path(parent_directory, repo_path)
+    ghclass::local_repo_clone(
+        repo_path,
+        fs::path_dir(project_folder)
+    )
+}
 
-fs::dir_ls(type = "directory") %>%
+course_team_repos <- org_repos(org_gh_course_name)
+fs::dir_create(fs::path("~", "Desktop", org_gh_course_name))
+course_team_repos %>%
+    purrr::walk(clone_team_projects,
+                parent_directory = fs::path("~", "Desktop"))
+
+# Pull from GitHub repositories
+fs::dir_ls(fs::path("~", "Desktop", org_gh_course_name),
+           type = "directory") %>%
     purrr::walk(local_repo_pull)
