f73bd144d3572b07c54dcd3e15b0505044137924
Luke W. Johnston <lwjohnst@gmail.com>
2022-03-10 23:09:15 +0100

    new way of managing contributor info and archiving?

--- R/generate-zenodo-metadata.R
+++ R/generate-zenodo-metadata.R
@@ -3,6 +3,11 @@ library(tidyverse)
 library(zen4R)
 library(gert)
 
+course_date <- "2022-03"
+
+# system("git shortlog -sne HEAD", intern = TRUE)
+# gert::git_info()
+
 authors_df <- desc_get_field("Authors@R") %>%
     str_remove_all('("|\\(|\\))') %>%
     str_replace_all(",", "\n") %>%
@@ -27,13 +32,14 @@ authors_df <- desc_get_field("Authors@R") %>%
 #     str_remove_all("\\n") %>%
 #     str_replace_all(" +", " ")
 
-repo_version <- str_c("v", as.character(desc::desc_get_version()))
+repo_version <- str_c("v", str_replace(course_date, "-", "."))
 version_tag <- git_tag_create(
     name = repo_version,
     message = "Learning module updated based on JOSE reviews and accepted for publication."
 )
 git_push()
 git_tag_push(repo_version)
+if (interactive()) browseURL("https://gitlab.com/rostools/r-cubed/-/releases/new")
 tag_archive_file <- str_c("r-cubed-", repo_version, ".zip")
 git_archive_zip(tag_archive_file)
 
