7dbff1e2c85450afea72e384b3e3897e28c822ff
Luke W. Johnston <lwjohnst@gmail.com>
2023-01-03 23:04:10 +0100

    don't need with quarto

--- R/functions.R
+++ R/functions.R
@@ -1,18 +1,1 @@
-
-#' Include a hidden, but clickable, section of details that is for instructors.
-#'
-#' @param .text Text to include in the details to instructors.
-#'
-#' @return a text surrounded by the HTML tag `details`.
-#'
-details_for_instructors <- function(.text) {
-    glue::glue("
-
-    <details style='margin-bottom: 1rem'><summary><strong><em>For instructors: Click for details.</em></strong></summary>
-    <blockquote><p>
-    {.text}
-    </p></blockquote>
-    </details>
-
-    ")
-}
+palette_git_commit <- '`Ctrl-Shift-P`, then type "commit"'
